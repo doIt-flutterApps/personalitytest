c87cdec5b09abe91c0509a8b661396a79c2b06ae
doIt-flutterApps <doit.flutterapps@gmail.com>
2025-04-27 11:34:41 +0900

    03장 제미나이 코드 수정

--- lib/main/mainlist_page.dart
+++ lib/main/mainlist_page.dart
@@ -135,7 +135,7 @@ class _MainPage extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: bannerUse ? AppBar(title: Text(welcomeTitle)) : null,
+      // appBar: bannerUse ? AppBar(title: Text(welcomeTitle)) : null,
       body: FutureBuilder<List<String>>(
         // Future 타입 명시하기
         future: loadAsset(),
@@ -158,11 +158,13 @@ class _MainPage extends State<MainPage> {
                     );
                     return InkWell(
                       child: SizedBox(
-                        height: remoteConfig.getInt('item_height').toDouble(),
+                        height: 150, // remoteConfig.getInt('item_height').toDouble(),
                         child: Card(
                           color: Colors.amber,
-                          child: Text('질문: ${item['title']}'),
-                        ),
+                          // child: Align(
+                          //     alignment: Alignment.center,
+                              child: Text('${item['title']}')),
+                        // ),
                       ),
                       onTap: () async {
                         await FirebaseAnalytics.instance
@@ -197,55 +199,55 @@ class _MainPage extends State<MainPage> {
           }
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          FirebaseDatabase database = FirebaseDatabase.instance;
-          DatabaseReference _testRef = database.ref('test');
-          _testRef.push().set("""{
-"title": "당신이 좋아하는 애완동물은?",
-"question": "당신이 무인도에 도착했는데, 마침 떠내려온 상자를 열었을 때 보이는 이것은?",
-"selects": [
-"생존 키트",
-"휴대폰",
-"텐트",
-"무인도에서 살아남기"
-],
-"answer": [
-"당신은 현실주의! 동물은 안 키운다!!",
-"당신은 늘 함께 있는 걸 좋아하는 강아지",
-"당신은 같은 공간을 공유하는 고양이",
-"당신은 낭만을 좋아하는 앵무새"
-]
-}""");
-          _testRef.push().set("""{
-"title": "5초 MBTI I/E 편",
-"question": "친구와 함께 간 미술관 당신이라면?",
-"selects": [
-"말이 많아짐",
-"생각이 많아짐"
-],
-"answer": [
-"당신의 성향은 E",
-"당신의 성향은 I"
-]
-}""");
-          _testRef.push().set("""{
-"title": "당신은 어떤 사랑을 하고 싶나요?",
-"question": "목욕을 할 때 가장 먼저 비누칠을 하는 곳은?",
-"selects": [
-"머리",
-"상체",
-"하체"
-],
-"answer": [
-"당신은 자만추를 추천해요.",
-"당신은 소개팅에서 새로운 사람을 소개받는 걸 좋아합니다.",
-"당신은 길 가다가 우연히 지나친 그런 인연을 좋아합니다."
-]
-}""");
-        },
-      ), // floatingActionButton
+//       floatingActionButton: FloatingActionButton(
+//         child: const Icon(Icons.add),
+//         onPressed: () {
+//           FirebaseDatabase database = FirebaseDatabase.instance;
+//           DatabaseReference _testRef = database.ref('test');
+//           _testRef.push().set("""{
+// "title": "당신이 좋아하는 애완동물은?",
+// "question": "당신이 무인도에 도착했는데, 마침 떠내려온 상자를 열었을 때 보이는 이것은?",
+// "selects": [
+// "생존 키트",
+// "휴대폰",
+// "텐트",
+// "무인도에서 살아남기"
+// ],
+// "answer": [
+// "당신은 현실주의! 동물은 안 키운다!!",
+// "당신은 늘 함께 있는 걸 좋아하는 강아지",
+// "당신은 같은 공간을 공유하는 고양이",
+// "당신은 낭만을 좋아하는 앵무새"
+// ]
+// }""");
+//           _testRef.push().set("""{
+// "title": "5초 MBTI I/E 편",
+// "question": "친구와 함께 간 미술관 당신이라면?",
+// "selects": [
+// "말이 많아짐",
+// "생각이 많아짐"
+// ],
+// "answer": [
+// "당신의 성향은 E",
+// "당신의 성향은 I"
+// ]
+// }""");
+//           _testRef.push().set("""{
+// "title": "당신은 어떤 사랑을 하고 싶나요?",
+// "question": "목욕을 할 때 가장 먼저 비누칠을 하는 곳은?",
+// "selects": [
+// "머리",
+// "상체",
+// "하체"
+// ],
+// "answer": [
+// "당신은 자만추를 추천해요.",
+// "당신은 소개팅에서 새로운 사람을 소개받는 걸 좋아합니다.",
+// "당신은 길 가다가 우연히 지나친 그런 인연을 좋아합니다."
+// ]
+// }""");
+//         },
+//       ), // floatingActionButton
     );
   }
 }
